1ebdd142bae16a016246b5fdc3c6a660b77b0a06
Rublov Viacheslav <timmy31101994@gmail.com>
2022-07-20 17:11:44 +0300

    HW_M4P3

---
+++ DbModel/Artist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_M4P3
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string InstagrameUrl {get; set; }
+        public virtual List<ArtistSong> ArtistSongs { get; set; }
+
+    }
+}
---
+++ Migrations/20220720140149_InitCreate.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HW_M4P3.Migrations
+{
+    public partial class InitCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artist",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Phone = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    InstagrameUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artist", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Song",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Song", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Song_Genre_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistSong",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArtistId = table.Column<int>(type: "int", nullable: false),
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistSong", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Artist_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artist",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Song_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Song",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artist",
+                columns: new[] { "Id", "DateOfBirth", "Email", "InstagrameUrl", "Name", "Phone" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1995, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "slipknot@gmail.com", "slipknot", "Slipknot", 11111111 },
+                    { 2, new DateTime(1994, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "rammstein@gmail.com", "rammstein", "Rammstein", 2222222 },
+                    { 3, new DateTime(2008, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "oxxxymiron@mail.ru", "oxxxymiron", "Oxxxymiron", 33333333 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genre",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Hard rock" },
+                    { 2, "Pop" },
+                    { 3, "Country" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Song",
+                columns: new[] { "Id", "Duration", "GenreId", "ReleaseDate", "Title" },
+                values: new object[] { 1, 300000, 1, new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sonne" });
+
+            migrationBuilder.InsertData(
+                table: "Song",
+                columns: new[] { "Id", "Duration", "GenreId", "ReleaseDate", "Title" },
+                values: new object[] { 2, 300000, 1, new DateTime(2009, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ich tu dir weh" });
+
+            migrationBuilder.InsertData(
+                table: "Song",
+                columns: new[] { "Id", "Duration", "GenreId", "ReleaseDate", "Title" },
+                values: new object[] { 3, 300000, 2, new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Organization" });
+
+            migrationBuilder.InsertData(
+                table: "ArtistSong",
+                columns: new[] { "Id", "ArtistId", "SongId" },
+                values: new object[] { 1, 2, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ArtistSong",
+                columns: new[] { "Id", "ArtistId", "SongId" },
+                values: new object[] { 2, 2, 2 });
+
+            migrationBuilder.InsertData(
+                table: "ArtistSong",
+                columns: new[] { "Id", "ArtistId", "SongId" },
+                values: new object[] { 3, 3, 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistSong_ArtistId",
+                table: "ArtistSong",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistSong_SongId",
+                table: "ArtistSong",
+                column: "SongId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Song_GenreId",
+                table: "Song",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistSong");
+
+            migrationBuilder.DropTable(
+                name: "Artist");
+
+            migrationBuilder.DropTable(
+                name: "Song");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
